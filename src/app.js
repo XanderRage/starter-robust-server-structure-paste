9c8c7022fb85e2d68ea83862dbc8a8e178218f05
Alex Novoa <cyberxan9@gmail.com>
2024-05-25 11:08:52 -0500

    upto advanced tips

--- src/app.js
+++ src/app.js
@@ -1,44 +1,11 @@
-app.use(express.json())
-
 const express = require("express");
 const app = express();
-
-// TODO: Follow instructions in the checkpoint to implement ths API.
 const pastes = require("./data/pastes-data");
+const pastesRouter = require("./pastes/pastes.router");
 
-app.use("/pastes/:pasteId", (req, res, next) => {
-  const { pasteId } = req.params;
-  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+app.use(express.json());
 
-  if (foundPaste) {
-    res.json({ data: foundPaste });
-  } else {
-    next(`Paste id not found: ${pasteId}`);
-  }
-});
-
-app.get("/pastes", (req, res) => {
-  res.json({ data: pastes });
-});
-
-// Variable to hold the next ID
-// Because some IDs may already be used, find the largest assigned ID
-let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
-
-app.post("/pastes", (req, res, next) => {
-  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
-  const newPaste = {
-    id: ++lastPasteId, // Increment last ID, then assign as the current ID
-    name,
-    syntax,
-    exposure,
-    expiration,
-    text,
-    user_id,
-  };
-  pastes.push(newPaste);
-  res.json({ data: newPaste });
-});
+app.use("/pastes", pastesRouter); // Note: app.use
 
 // Not found handler
 app.use((request, response, next) => {
@@ -46,9 +13,10 @@ app.use((request, response, next) => {
 });
 
 // Error handler
-app.use((error, request, response, next) => {
+app.use((error, req, res, next) => {
   console.error(error);
-  response.send(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  res.status(status).json({ error: message });
 });
 
 module.exports = app;
